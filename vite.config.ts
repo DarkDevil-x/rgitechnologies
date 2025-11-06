c6803241c90c8029ee5d247df3e19b4ea73a6057
prernachauhan28 <chauhanprerna2804@gmail.com>
2025-11-06 22:55:57 +0530

    image reduce

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath } from 'url'
+import viteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),viteImagemin({
+    mozjpeg: { quality: 75 },
+    pngquant: { quality: [0.7, 0.9] },
+    webp: { quality: 75 },
+  }),],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
